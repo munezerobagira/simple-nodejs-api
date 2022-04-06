cafdd55ba72191e42dfdb100cf95fbc43d8caec1
Sostene Munezero Bagira <munezerobagira@gmail.com>
2022-04-06 10:15:10 +0200

    added controllers

--- src/routes.js
+++ src/routes.js
@@ -1,53 +1,15 @@
 import express from "express";
-import Post from "./models/Post";
+import {
+  getPosts,
+  singlePost,
+  createPost,
+  updatePost,
+  deletePost,
+} from "./controller/postController";
 const router = express.Router();
-router.get("/posts", async (req, res) => {
-  const posts = await Post.find();
-  res.send(posts);
-});
-router.post("/posts", async (req, res) => {
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content,
-  });
-  await post.save();
-  res.send(post);
-});
-router.get("/posts/:id", async (req, res) => {
-  try {
-    const post = await Post.findOne({ _id: req.params.id });
-    res.send(post);
-  } catch {
-    res.status(404);
-    res.send({ error: "Post doesn't exist!" });
-  }
-});
-router.patch("/posts/:id", async (req, res) => {
-  try {
-    const post = await Post.findOne({ _id: req.params.id });
-
-    if (req.body.title) {
-      post.title = req.body.title;
-    }
-
-    if (req.body.content) {
-      post.content = req.body.content;
-    }
-
-    await post.save();
-    res.send(post);
-  } catch {
-    res.status(404);
-    res.send({ error: "Post doesn't exist!" });
-  }
-});
-router.delete("/posts/:id", async (req, res) => {
-  try {
-    await Post.deleteOne({ _id: req.params.id });
-    res.status(204).send();
-  } catch {
-    res.status(404);
-    res.send({ error: "Post doesn't exist!" });
-  }
-});
+router.get("/posts", getPosts);
+router.post("/posts", createPost);
+router.get("/posts/:id", singlePost);
+router.patch("/posts/:id", updatePost);
+router.delete("/posts/:id", deletePost);
 export default router;
